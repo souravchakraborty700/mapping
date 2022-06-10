9f40f5a29820f3adaa90b462b255544f3f759020
Sourav Chakraborty <souravchakraborty700@gmail.com>
2022-06-10 23:06:00 +0530

    updated

--- map.py
+++ map.py
@@ -1,14 +1,32 @@
 import folium
 import pandas
-
+ 
 data = pandas.read_csv("Volcanoes.txt")
-
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
-map = folium.Map(location=[22.49, 88.43], zoom_start = 6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup("Kestopur Map")
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="I am a Marker", icon = folium.Icon(color = "green")))
+def color_producer(eleveter):
+    if eleveter<1000:
+        return "green"
+    elif eleveter >= 1000 and eleveter <2000:
+        return "orange"
+    else:
+        return "red"
+ 
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+ 
+map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="Stamen Terrain")
+fg = folium.FeatureGroup(name = "My Map")
+ 
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), color="grey", fill_opecity=0.1, fill_color = color_producer(el)))
+ 
 map.add_child(fg)
 map.save("Map.html")
