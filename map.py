5f471c78dda61d38f16c37d943ab5eec7b3df734
Sourav Chakraborty <souravchakraborty700@gmail.com>
2022-06-10 20:19:53 +0530

    Added Volcanoes

--- map.py
+++ map.py
@@ -1,6 +1,14 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[22.49, 88.43], zoom_start = 6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup("Kestopur Map")
-fg.add_child(folium.Marker(location=[22.49, 88.43], popup="I am a Marker", icon = folium.Icon(color = "green")))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="I am a Marker", icon = folium.Icon(color = "green")))
 map.add_child(fg)
 map.save("Map.html")
