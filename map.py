1f234fcefec746ea6aa19c1c520c27c2252c0681
Sourav Chakraborty <souravchakraborty700@gmail.com>
2022-06-11 20:10:41 +0530

    Added Polynomial Color

--- map.py
+++ map.py
@@ -28,5 +28,7 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), color="grey", fill_opecity=0.1, fill_color = color_producer(el)))
  
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding="utf-8-sig").read(), style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"]["POP2005"]<20000000 else "red" }))
+
 map.add_child(fg)
 map.save("Map.html")
